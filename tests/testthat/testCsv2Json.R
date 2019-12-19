7d33c659d6c293915e7b8a6cd032e01ff490eafd
Jochen Weile <jochenweile@gmail.com>
2019-12-19 12:07:24 -0500

    moved test data

--- tests/testthat/testCsv2Json.R
+++ tests/testthat/testCsv2Json.R
@@ -3,7 +3,11 @@ context("parameter parsing")
 
 test_that("CSV2JSON", {
 
-	infile <- "/test/test_that/paramtest.csv"
+	# infile <- "test/test_that/paramtest.csv"
+	infile <- system.file("testdata/paramtest.csv",
+		package = "tileseqMave",
+		mustWork = TRUE
+	)
 	outfile <- tempfile()
 	csvParam2Json(infile,outfile)
 
