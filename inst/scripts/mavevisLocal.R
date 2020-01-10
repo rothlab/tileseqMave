5a0f58c52ebb62a029e31215d483250a6822558f
Jochen Weile <jochenweile@gmail.com>
2020-01-10 16:50:30 -0500

    move scripts to installation folder

---
+++ inst/scripts/mavevisLocal.R
@@ -0,0 +1,155 @@
+#!/usr/bin/Rscript
+
+# Copyright (C) 2018  Jochen Weile, Roth Lab
+#
+# This file is part of tileseqMave.
+#
+# tileseqMave is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# tileseqMave is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with tileseqMave.  If not, see <https://www.gnu.org/licenses/>.
+
+##############################################
+#This is a command line tool for locally running mavevis
+##############################################
+
+options(stringsAsFactors=FALSE)
+
+library(mavevis)
+library(hgvsParseR)
+library(yogitools)
+
+#get input file argument
+infile <- getArg("infile",required=TRUE)
+#and check that the file exists and can be read
+if (!canRead(infile)) {
+	stop("The given input file does not exist or cannot be read!")
+}
+if (!grepl("\\.csv$",infile)) {
+	stop("The given input file is not a CSV file!")
+}
+
+#get the uniprot id argument and validate
+uniprot <- getArg("uniprot",required=TRUE)
+uniprotRX <- "^[OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}$"
+if (!grepl(uniprotRX,uniprot)) {
+	stop("The given Uniprot Accession is invalid!")
+}
+#TODO: Validate internet connection
+
+#get the PDB argument, parse and validate it.
+pdbArg <- getArg("pdb",default=NULL)
+if (!is.null(pdbArg)) {
+	pdb <- strsplit(pdbArg,";")[[1]]
+	pdbIds <- sapply(strsplit(pdb,"#"),`[[`,1)
+	pdbChains <- sapply(strsplit(pdb,"#"),`[[`,2)
+
+	if (!all(grepl("^[0-9][A-Za-z0-9]{3}$",pdbIds))) {
+		stop("One or more of the given PDB IDs is invalid!")
+	}
+	if (!all(grepl("^[A-Z]{1}$",pdbChains))) {
+		stop("One or more of the PDB chain identifiers is invalid!")
+	}
+}
+
+
+pdffile <- getArg("pdfOut",default=NULL)
+if (is.null(pdffile)) {
+	pdffile <- sub("\\.csv",".pdf",infile)
+}
+
+
+cat("Reading and parsing input data...")
+indata <- read.csv(infile)
+mutdata <- parseHGVS(indata$hgvs_pro,aacode=1)
+mutdata[which(mutdata$variant=="*"),"type"] <- "nonsense"
+data <- cbind(mutdata,indata[,-1])
+
+cat("done!\n")
+
+#derive WT sequence
+cat("Deriving WT sequence...\n")
+ancestrals <- with(data,tapply(ancestral,start,unique))
+wt.aa <- ancestrals[as.character(1:max(data$start))]
+wt.aa[[1]] <- "M"
+
+if (any(is.na(wt.aa))) {
+	warning("Unable to fully derive WT sequence! Defaulting to Uniprot sequence.")
+	wt.aa <- yogitools::toChars(mavevis::getUniprotSeq(uniprot))
+}
+
+td <- new.trackdrawer(length(wt.aa),nox=TRUE)
+
+if (!is.null(uniprot)) {
+	cons <- calc.conservation(uniprot)
+	td$add.constrack(cons)
+}
+
+if (!is.null(pdbArg)) {
+
+	strucfeats <- mapply(calc.strucfeats,pdbIds,pdbChains,SIMPLIFY=FALSE)
+
+	#consolidate secondary structure information from all structures
+	sscols <- lapply(strucfeats,`[`,,"secstruc")
+	fillup.max <- max(sapply(sscols,length))
+	sscols <- lapply(sscols,function(xs) c(xs,rep(NA,fillup.max-length(xs))))
+	if (length(sscols) > 1) {
+		ss.consensus <- apply(do.call(cbind,sscols),1,function(xs) if (!all(is.na(xs))) names(which.max(table(xs))) else NA)
+	} else {
+		ss.consensus <- sscols[[1]]
+	}
+
+	#consolidate SASA information from all structures
+	accols <- lapply(strucfeats,`[`,,"all.rel")
+	fillup.max <- max(sapply(sscols,length))
+	accols <- lapply(accols,function(xs) c(xs,rep(NA,fillup.max-length(xs))))
+	acc.consensus <- apply(do.call(cbind,accols),1,median,na.rm=TRUE)
+
+	td$add.ss.track(ss.consensus)
+	td$add.track(acc.consensus,"Rel. ASA","steelblue3")
+	for (sf in strucfeats) {
+		burial.columns <- which(grepl("rel.burial",colnames(sf)))
+		if (length(burial.columns) > 0) {
+			for (col in burial.columns) {
+				prot <- sub("rel.burial.","",colnames(sf)[[col]])
+				td$add.track(sf[,col],prot,"orange",maxVal=1)
+			}
+		}
+	}
+}
+
+cat("Drawing genophenogram...\n")
+
+#build genophenogram
+# img.width <- length(wt.aa) * 0.06 + 2.5
+img.width <- length(wt.aa) * 0.13 + 4
+img.height <- 4.5 + 0.13 * if(is.null(td)) 0 else td$num.tracks()
+
+pdf(pdffile,width=img.width,height=img.height)
+genophenogram(
+	wt.aa,
+	data$start,
+	data$variant,
+	data$score,
+	1,0,
+	error=data$se,
+	grayBack=TRUE,
+	img.width=img.width,
+	tracks=td
+)
+invisible(dev.off())
+cat("done\n")
+
+
+cat("\nScript completed successfully!\n")
+
+
+
---
+++ inst/scripts/refine.R
@@ -0,0 +1,209 @@
+#!/usr/bin/Rscript
+
+#################################################################################################
+# refine.R performs refinement based on the output of Joe's imputation webtool
+#
+# Usage: Rscript refine.R in=<path-to-input-file> [org=<path-to-detailed-scores>] out=<path-to-output-file> pdf=<path-to-pdf-output>
+# If no output parameters are provided, they will be generated automatically in the same 
+# folder as the input file.
+#
+# DEPENDENCIES: yogitools and hgvsParseR
+# To install these dependencies use devtools:
+# install.packages(devtools)
+# devtools::install_github("jweile/yogitools")
+# devtools::install_github("jweile/hgvsParseR")
+# devtools::install_github("jweile/mavevis")
+# The MaveVis dependency requires clustalo, dssp and freesasa to be installed as well!
+##################################################################################################
+
+options(stringsAsFactors=FALSE)
+library(yogitools)
+library(hgvsParseR)
+library(mavevis)
+library(hash)
+
+#define helper function for joining datasets weighted by stdev
+join.datapoints <- function(ms,sds) {
+	ws <- (1/sds)/sum(1/sds)
+	mj <- sum(ws*ms)
+	vj <- sum(ws*(sds^2+ms^2)) -mj^2
+	c(mj=mj,sj=sqrt(vj))
+}
+#define helper function to calculate RMSD
+calcRMSD <- function(a,b) {
+	sqrt(mean((a-b)^2,na.rm=TRUE))
+}
+
+#Read command line arguments
+infile <- getArg("in",required=TRUE)
+if (!file.exists(infile)) {
+	stop(infile," does not exist!")
+}
+orgfile <- getArg("org",default=NULL)
+if (!is.null(orgfile)) {
+	if (!file.exists(orgfile)) {
+		stop(orgfile," does not exist!")
+	}
+}
+outfile <- getArg("out",default=sub("\\.csv$","_refined_mavedb.csv",infile))
+pdffile <- getArg("pdf",default=sub("\\.csv$","_refined_mavedb.pdf",infile))
+
+#whether or not to use RMSD as the confidence gauge for imputed values
+useRMSD <- as.logical(getArg("useRMSD",default=TRUE))
+
+#cutoff for how many experimental values must exist in a column to include imputation output
+columnCutoff <- as.integer(getArg("columnCutoff",default=3))
+
+#Read input file
+cat("Reading input file...")
+indata <- read.csv(infile)
+#Check that all required columns are present
+required <- c(
+	"aa_ref","aa_pos","aa_alt","num_replicates",
+	"fitness","fitness_sd","fitness_imputed","fitness_imputed_se"
+)
+if (!all(required %in% colnames(indata))) {
+	missing <- setdiff(required,colnames(indata))
+	stop("Input file ",infile," is missing the following column(s): ",paste(missing,collapse=", "))
+}
+
+if (!is.null(orgfile)) {
+	org <- read.csv(orgfile)
+	#Check that all required columns are present
+	required <- c(
+		"hgvsp","hgvsc","score","sd","se"
+	)
+	if (!all(required %in% colnames(org))) {
+		missing <- setdiff(required,colnames(org))
+		stop("Input file ",orgfile," is missing the following column(s): ",paste(missing,collapse=", "))
+	}
+	hgvsLookup <- with(org,hash(hgvsp,strsplit(hgvsc," ")))
+}
+
+
+cat("done\n")
+# with(indata,topoScatter(
+# 	fitness_sd/sqrt(2),fitness_imputed_se,
+# 	xlab="experiment se",ylab="imputation se",
+# 	resolution=80
+# ))
+# abline(0,1,col="gray")
+
+#Construct the HGVS strings
+cat("Constructing HGVS descriptors...")
+hgvsp <- new.hgvs.builder.p(aacode=3)
+hgvss <- sapply(1:nrow(indata),function(i) with(indata[i,],{
+	if (aa_alt=="_") {
+		hgvsp$substitution(aa_pos,aa_ref,"*")
+	} else if (aa_alt==aa_ref) {
+		hgvsp$synonymous(aa_pos,aa_ref)
+	} else {
+		hgvsp$substitution(aa_pos,aa_ref,aa_alt)
+	}
+}))
+cat("done\n")
+
+if (exists("hgvsLookup")) {
+	hgvsc <- lapply(hgvss, function(h) {
+		if (has.key(h,hgvsLookup)) {
+			hgvsLookup[[h]]
+		} else {
+			"c.?"
+		}
+	})
+}
+
+#Determine which columns exceed the cutoff for imputation to be trusted
+valsPerCol <- with(indata,tapply(fitness,aa_pos,function(xs)sum(!is.na(xs))))
+trustedColumns <- as.integer(names(which(valsPerCol > columnCutoff)))
+untrustedColumns <- as.integer(names(which(valsPerCol <= columnCutoff)))
+if (length(untrustedColumns) > 0) {
+	cat("Exluding imputation for columns",paste(untrustedColumns,collapse=", "),"not meeting cutoff.")
+}
+
+
+
+cat("Stretching predicted values to [0:1] interval...")
+#find the numerical interval that the imputation filled
+imputedRange <- range(indata$fitness_imputed,na.rm=TRUE)
+#stretch the predictions to the full [0;1] range
+indata$stretched <- (indata$fitness_imputed - imputedRange[[1]])/(imputedRange[[2]]-imputedRange[[1]])
+cat("done\n")
+
+cat("Calculating prediction RMSD...")
+#determine the RMSD of the prediction
+rmsd <- with(indata,calcRMSD(stretched,fitness))
+cat("done\n")
+
+cat("Running refinement...")
+#and refine based on that RMSD
+refined <- as.df(lapply(1:nrow(indata),function(i) with(indata[i,],{
+	impse <- if (is.na(fitness_imputed_se)) rmsd else fitness_imputed_se
+	#if no imputation exists, or the column isn't trusted, use the experimental value (or NA)
+	if (is.na(stretched) | !(aa_pos %in% trustedColumns)) {
+		c(mj=fitness,sj=fitness_sd)
+	#otherwise, if no experimental value exists, use imputation
+	} else if (is.na(fitness)) {
+		if (useRMSD) {
+			c(mj=stretched,sj=rmsd)
+		} else {
+			c(mj=stretched,sj=impse)
+		}
+	#or, by default, form the confidence-weighted average between both
+	} else {
+		if (useRMSD) {
+			out <- join.datapoints(ms=c(stretched,fitness),sds=c(rmsd,fitness_sd/sqrt(num_replicates)))
+			c(mj=out[["mj"]],sj=out[["sj"]]*sqrt(num_replicates))
+		} else {
+			imp.se.adj <- impse*sqrt(10)/sqrt(num_replicates)
+			exp.se <- fitness_sd/sqrt(num_replicates)
+			out <- join.datapoints(ms=c(stretched,fitness),sds=c(imp.se.adj,exp.se))
+			c(mj=out[["mj"]],sj=out[["sj"]]*sqrt(num_replicates))
+		}
+	}
+})))
+cat("done\n")
+
+#build output table
+numrep <- sapply(indata$num_replicates,function(x) if(is.na(x)) 1 else x)
+outdata <- cbind(hgvss,refined,refined$sj/sqrt(numrep))
+colnames(outdata) <- c("hgvs_pro","score","sd","se")
+
+if (exists("hgvsc")) {
+	outdata2 <- do.call(rbind,lapply(1:nrow(outdata), function(i) {
+		cbind(hgvs_nt=hgvsc[[i]],outdata[i,])
+	}))
+}
+
+cat("Writing output table...")
+if (exists("outdata2")) {
+	write.csv(outdata2,outfile,row.names=FALSE)
+} else {
+	write.csv(outdata,outfile,row.names=FALSE)
+}
+cat("done\n")
+
+cat("Drawing genophenogram...")
+#derive WT sequence
+ancestrals <- with(indata,tapply(aa_ref,aa_pos,unique))
+wt.aa <- ancestrals[as.character(1:max(indata$aa_pos))]
+wt.aa[[1]] <- "M"
+
+#build genophenogram
+img.width <- length(wt.aa) * 0.06 + 2.5
+pdf(pdffile,width=img.width,height=2.5)
+genophenogram(
+	wt.aa,
+	indata$aa_pos,
+	sapply(indata$aa_alt,function(x) if (x == "_") "*" else x),
+	outdata$score,
+	1,0,
+	error=outdata$se,
+	grayBack=TRUE,
+	img.width=img.width
+)
+invisible(dev.off())
+cat("done\n")
+
+
+cat("\nScript completed successfully!\n")
