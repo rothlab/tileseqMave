0a0c186657051aad8302b50cb942790261c570c4
Jochen Weile <jochenweile@gmail.com>
2021-04-22 15:02:58 -0400

    multi-threading for calibratePhred

--- inst/scripts/calibratePhred.R
+++ inst/scripts/calibratePhred.R
@@ -32,6 +32,7 @@ library(tileseqMave)
 library(argparser)
 library(bitops)
 library(yogilog)
+library(parallel)
 
 p <- argparser::arg_parser(
   "Calibrate PHRED scores in a WTctrl SAM file to their empirical error probabilities",
@@ -45,7 +46,7 @@ p <- add_argument(p, "--logfile", help="log file. Defaults to 'calibratePhred.lo
 p <- add_argument(p, "--srOverride", help="Manual override to allow singleton replicates. USE WITH EXTREME CAUTION!",flag=TRUE)
 p <- add_argument(p, "--maxReads", help="Maximum number of reads to process. (To finish faster)",default=1e5)
 p <- add_argument(p, "--silent", help="Turn off message printing to stdout",flag=TRUE)
-
+p <- add_argument(p, "--cores", default=6L, help="Number of CPU cores to use")
 args <- parse_args(p)
 
 
@@ -100,136 +101,202 @@ flagMasks <- c(
   failQC=0x200, duplicate=0x400, supplementary=0x800
 )
 
-#read sam file
-logger$info("Processing SAM file")
-
-#set up global count matrix to be filled during chunk processing
-counts <- matrix(0,nrow=length(phredChars),ncol=2,dimnames=list(phredChars,c("err","tot")))
-
-
-#TODO: may be better to pipe 'shuf -n $maxReads <file>"
-con <- file(sam.file,open="r")
-#read file in 1000 line chunks
-chunk <- 0
-while(length(lines <- readLines(con,1000))>0) {
-  
-  #read chunk as into table via textConnection
-  tcon <- textConnection(lines)
-  sam <- read.delim(tcon,header=FALSE,stringsAsFactors=FALSE)
-  close(tcon)
-  
-  colnames(sam) <- c(
+#helper function to parse the SAM tab-delimited format
+#read.delim doesn't work, because lines can have differing
+#numbers of columns!
+parseSpecialTab <- function(lines) {
+  splits <- strsplit(lines,"\\t")
+  maxCols <- max(sapply(splits,length))
+  df <- as.df(lapply(splits, function(elem) {
+    if (length(elem) < maxCols) {
+      c(elem,rep(NA,maxCols-length(elem)))
+    } else elem
+  }))
+  colnames(df) <- c(
     "cname","flag","rname","pos","mapq","cigar","mrnm","mpos",
-    "isize","seq","qual","tags", 13:ncol(sam)
+    "isize","seq","qual","tags", 13:maxCols
   )
-  
-  #parse CIGAR strings
-  cigar <- yogitools::global.extract.groups(sam$cigar,"(\\d+)([SHNMDIP]{1})")
-  
-  #parse FLAGS
-  flags <- do.call(rbind,lapply(sam$flag,function(x)bitops::bitAnd(x,flagMasks)>0))
-  colnames(flags) <- names(flagMasks)
-  flags <- to.df(flags)
-  
-  #inner loop to process individual lines in the chunk
-  for (i in 1:nrow(sam)) {
-    
-    #if the segment is unmapped, ignore it.
-    if (is.na(flags[i,"segmentUnmapped"]) || flags[i,"segmentUnmapped"]) {
-      next
-    }
-    
-    #the read sequence
-    rseq <- sam[i,"seq"]
-    #the phred quality track
-    qual <- sam[i,"qual"]
-    
-    #a positional cursor keeping track of where we are in the template sequence
-    tcursor <- sam[i,"pos"]
-    #a position cursor for where we are in the read sequence
-    rcursor <- 1
-    
-    #string buffers for the expanded template, read, and quality alignments
-    tbuffer <- ""
-    rbuffer <- ""
-    qbuffer <- ""
-    
-    #load the parsed cigar string
-    cig <- cigar[[i]]
-    
-    #variable for keeping track of error states in the inner loop
-    cigarError <- FALSE
-    
-    #process all positions in this line
-    for (j in 1:nrow(cig)) {
-      #k is the length of the current cigar element (i.e. match length etc)
-      k <- as.integer(cig[j,1])
-      #op is the cigar operation (i.e. match / deletion / insertion etc)
-      op <- cig[j,2]
-      #build the alignment in the string buffers based on the operations
-      switch(op,
-             M={#match/mismatch
-               tbuffer <- paste0(tbuffer,substr(wtseq,tcursor,tcursor+k-1))
-               rbuffer <- paste0(rbuffer,substr(rseq,rcursor,rcursor+k-1))
-               qbuffer <- paste0(qbuffer,substr(qual,rcursor,rcursor+k-1))
-               tcursor <- tcursor+k
-               rcursor <- rcursor+k
-             },
-             D={#deletion
-               spaces <- paste(rep(" ",k),collapse="")
-               tbuffer <- paste0(tbuffer,substr(wtseq,tcursor,tcursor+k-1))
-               rbuffer <- paste0(rbuffer,spaces)
-               qbuffer <- paste0(qbuffer,spaces)
-               tcursor <- tcursor+k
-             },
-             I={#insertion
-               spaces <- paste(rep(" ",k),collapse="")
-               tbuffer <- paste0(tbuffer,spaces)
-               rbuffer <- paste0(rbuffer,substr(rseq,rcursor,rcursor+k-1))
-               qbuffer <- paste0(qbuffer,substr(qual,rcursor,rcursor+k-1))
-               rcursor <- rcursor+k
-             },
-             S={#soft clipping
-               rcursor <- rcursor+k
-             },
-             H={#hard clipping
-               rcursor <- rcursor+k
-             },
-             {#other
-               logWarn("Unsupproted cigar character: ",op," in row ",chunk+i)
-               cigarError <- TRUE
-             }
-      )
-    }
-    
-    if (cigarError) {
-      next
-    }
-    
-    matches <- data.frame(t=toChars(tbuffer),r=toChars(rbuffer),q=toChars(qbuffer))
-    matches$err <- with(matches,t != r)
-    if (sum(matches$err) > 10) {
-      logWarn("Suspicious error rate in row ",chunk+i)
-    }
-    icounts <- do.call(rbind,with(matches,tapply(err,q,function(e)c(sum(e),length(e)))))
-    counts[rownames(icounts),] <- counts[rownames(icounts),] + icounts
-    
-    # setTxtProgressBar(pb, i)
-    
-  }
-  
-  # close(pb)
-  chunk <- chunk+length(lines)
-  logInfo("Processed",chunk,"lines...")
-  
-  if (chunk > maxReads) {
-    logInfo("Exceeded maxReads parameter. Terminating...")
-    break
-  }
+  df$flag <- as.integer(df$flag)
+  df$pos <- as.integer(df$pos)
+  df$mapq <- as.integer(df$mapq)
+  df$mpos <- as.integer(df$mpos)
+  df$isize <- as.integer(df$isize)
+  df
 }
-close(con)
 
-logger$info("Writing results to file ",outfile)
+#read sam file
+logInfo("Processing SAM file")
+
+# TODO: it may be useful to sample random lines e.g. 
+# shuf -n $maxReads <file>
+# also, potentially enriching for matches using samtools?
+
+#set up multithreading
+nthreads <- args$cores
+
+#scope out the file size
+#the sam files are roughly 430-465 bytes per line, but we will purposefully
+#overestimate this as 500bytes per line to get a conservative number
+totalLines <- round(file.info(sam.file)$size/500)
+#if the approximate number is close to our cutoff, we want a more precise number
+if (totalLines < 1.2*maxReads) {
+  totalLines <- as.integer(strsplit(
+    system2("wc",c("-l",sam.file),stdout=TRUE),
+    "\\s+"
+  )[[1]][[1]])
+} 
+
+#if the file is smaller than our maximum line number, it becomes our new maximum
+maxReads <- min(maxReads,totalLines)
+#subdivide the task into chunks (the number of lines to process by each thread)
+chunkSize <- floor(maxReads/nthreads)
+
+#start multiple threads
+result <- mclapply(1:nthreads,function(threadi) {
+  
+  startLine <- chunkSize*(threadi-1)+1
+  endLine <- startLine+chunkSize-1
+  
+  #set up global count matrix to be filled during chunk processing
+  counts <- matrix(0,nrow=length(phredChars),ncol=2,dimnames=list(phredChars,c("err","tot")))
+  
+  #keep track of how many lines were processed or errors were encountered
+  events <- yogitools::new.counter()
+  
+  # con <- file(sam.file,open="r")
+  # pipe(sprintf("tail +%d %s|head -%d",startLine,sam.file,chunkSize))
+  con <- pipe(sprintf("sed -n %d,%dp %s",startLine,endLine,sam.file),open="r")
+
+  #read file in 1000 line chunks
+  while(length(lines <- readLines(con,1000))>0) {
+    
+    #parse lines
+    sam <- parseSpecialTab(lines)
+    
+    #parse CIGAR strings
+    cigar <- yogitools::global.extract.groups(sam$cigar,"(\\d+)([SHNMDIP]{1})")
+    
+    #parse unmapped flag
+    unmapped <- bitops::bitAnd(sam$flag,flagMasks[["segmentUnmapped"]])>0
+    
+    #inner loop to process individual lines in the chunk
+    for (i in 1:nrow(sam)) {
+      
+      #if the segment is unmapped, ignore it.
+      if (is.na(unmapped[[i]]) || unmapped[[i]]) {
+        next
+      }
+      
+      #the read sequence
+      rseq <- sam[i,"seq"]
+      #the phred quality track
+      qual <- sam[i,"qual"]
+      
+      #a positional cursor keeping track of where we are in the template sequence
+      tcursor <- sam[i,"pos"]
+      #a position cursor for where we are in the read sequence
+      rcursor <- 1
+      
+      #string buffers for the expanded template, read, and quality alignments
+      tbuffer <- ""
+      rbuffer <- ""
+      qbuffer <- ""
+      
+      #load the parsed cigar string
+      cig <- cigar[[i]]
+      
+      #variable for keeping track of error states in the inner loop
+      cigarError <- FALSE
+      
+      #process all positions in this line
+      for (j in 1:nrow(cig)) {
+        #k is the length of the current cigar element (i.e. match length etc)
+        k <- as.integer(cig[j,1])
+        #op is the cigar operation (i.e. match / deletion / insertion etc)
+        op <- cig[j,2]
+        #build the alignment in the string buffers based on the operations
+        switch(op,
+               M={#match/mismatch
+                 tbuffer <- paste0(tbuffer,substr(wtseq,tcursor,tcursor+k-1))
+                 rbuffer <- paste0(rbuffer,substr(rseq,rcursor,rcursor+k-1))
+                 qbuffer <- paste0(qbuffer,substr(qual,rcursor,rcursor+k-1))
+                 tcursor <- tcursor+k
+                 rcursor <- rcursor+k
+               },
+               D={#deletion
+                 spaces <- paste(rep(" ",k),collapse="")
+                 tbuffer <- paste0(tbuffer,substr(wtseq,tcursor,tcursor+k-1))
+                 rbuffer <- paste0(rbuffer,spaces)
+                 qbuffer <- paste0(qbuffer,spaces)
+                 tcursor <- tcursor+k
+               },
+               I={#insertion
+                 spaces <- paste(rep(" ",k),collapse="")
+                 tbuffer <- paste0(tbuffer,spaces)
+                 rbuffer <- paste0(rbuffer,substr(rseq,rcursor,rcursor+k-1))
+                 qbuffer <- paste0(qbuffer,substr(qual,rcursor,rcursor+k-1))
+                 rcursor <- rcursor+k
+               },
+               S={#soft clipping
+                 rcursor <- rcursor+k
+               },
+               H={#hard clipping
+                 rcursor <- rcursor+k
+               },
+               {#other
+                 # logWarn("Unsupproted cigar character: ",op," in row ",chunk+i)
+                 cigarError <- TRUE
+               }
+        )
+      }
+      
+      if (cigarError) {
+        events$inc("cigarError")
+        next
+      }
+      
+      matches <- data.frame(t=toChars(tbuffer),r=toChars(rbuffer),q=toChars(qbuffer))
+      matches$err <- with(matches,t != r)
+      if (sum(matches$err) > 10) {
+        events$inc("suspicious")
+      }
+      icounts <- do.call(rbind,with(matches,tapply(err,q,function(e)c(sum(e),length(e)))))
+      counts[rownames(icounts),] <- counts[rownames(icounts),] + icounts
+      
+    }
+    
+    events$add("processed",length(lines))
+  }
+  close(con)
+  
+  return(list(counts=counts,events=events))
+  
+},mc.cores=nthreads)
+
+#add all the count matrices together
+counts <- Reduce(`+`,lapply(result,`[[`,1))
+
+#join all the event logs together
+events <- new.counter()
+for (eventLog in sapply(result,function(res)res$events$export())) {
+  events$import.add(eventLog)
+}
+events <- events$ls()
+
+logInfo(sprintf("Successfully processed %d reads.",events$processed))
+
+if ("suspicious" %in% names(events)) {
+  logWarn(sprintf("%d reads had over 10 errors each!",events$suspicious))
+}
+
+totRate <- sum(counts[,1])/sum(counts[,2])
+highRate <- sum(counts[-(1:30),1])/sum(counts[-(1:30),2])
+logInfo(sprintf(
+  "Total error rate: %.02f%%; high quality error rate: %.02f%%",
+  100*totRate,100*highRate
+))
+
+logInfo("Writing results to file ",outfile)
 result <- data.frame(
   specification=phredProbs,observed=counts[,1]/counts[,2],
   errors=counts[,1],total=counts[,2]
@@ -238,34 +305,5 @@ rownames(result)[[1]] <- "del"
 
 write.csv(result,outfile)
 
-totRate <- sum(counts[,1])/sum(counts[,2])
-highRate <- sum(counts[-(1:10),1])/sum(counts[-(1:10),2])
-logInfo(sprintf(
-  "Total error rate: %.02f%%; high quality error rate: %.02f%%",
-  100*totRate,100*highRate
-))
-
-# fit <- coefficients(lm(log10(result[!is.na(result[,2]),2])~which(!is.na(result[,2]))))
-
-# idx <- which(!is.na(result[-1,2]))
-# 
-# pdffile <- sub(".sam$","_qualFreqs.pdf",basename(sam.file))
-# pdf(pdffile,11,6)
-# op <- par(mfrow=c(2,1),cex=0.9)
-# barcols <- c("gray","firebrick3")
-# xs <- barplot(t(as.matrix(result[-1,-3])),beside=TRUE,col=barcols,ylim=c(0,1.2),
-#               main="Sequencing error rates",xlab="PHRED",ylab="Error probabability"
-# )
-# text(colMeans(xs)[idx],result[idx+1,2],
-#      paste0("n=",trimws(format(result[idx+1,3],big.mark=","))),
-#      pos=3,cex=0.7
-# )
-# grid(NA,NULL)
-# legend("right",c("Specificiation","Empirical"),fill=barcols)
-# barplot(t(as.matrix(result[-1,-3])),beside=TRUE,col=barcols,log="y",
-#         xlab="PHRED",ylab="Error probabability"
-# )
-# grid(NA,NULL)
-# par(op)
-# dev.off()
+logInfo("Done!")
 
