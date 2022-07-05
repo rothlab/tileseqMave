7573de9cf413e9c8ad1491639eaa8a15c3ed4cca
Jochen Weile <jochenweile@gmail.com>
2022-07-05 15:39:13 -0400

    selfupdate script and version bump

---
+++ inst/scripts/selfupdate.R
@@ -0,0 +1,24 @@
+#!/usr/bin/env Rscript
+
+# Copyright (C) 2018  Jochen Weile, Roth Lab
+#
+# This file is part of tileseqMave.
+#
+# tileseqMave is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# tileseqMave is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with tileseqMave.  If not, see <https://www.gnu.org/licenses/>.
+
+##############################################
+# This is a self-update helper script
+##############################################
+
+remotes::install_github("jweile/tileseqMave")
