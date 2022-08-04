f67471e04952d2f662ddb214e0475f29f1be9922
Jochen Weile <jochenweile@gmail.com>
2022-08-04 14:45:19 -0400

    add printout of LLR level transitions

--- inst/scripts/calcLLR.R
+++ inst/scripts/calcLLR.R
@@ -27,7 +27,9 @@ p <- add_argument(p, "--posRange", help="Positional range within the map to be t
 p <- add_argument(p, "--outlierSuppression", help="Strength of outlier suppression to use. Between 0 and 1",default=0.0001)
 p <- add_argument(p, "--logfile", help="The desired log file location.",default="llr.log")
 p <- add_argument(p, "--iterations", help="The number of bootstrap iterations for determining LLR confidence interval",default=10000)
+p <- add_argument(p, "--printTransitions", help="Print evidence code transitions.",flag=TRUE)
 args <- parse_args(p)
+# args <- list(map="CHEK2_experimental.csv",reference="CHEK2_refVars_ClinvarPlusPlus.csv", outfile="b05/CHEK2_experimental_LLR_b05", bandwidth=0.5,kernel="epanechnikov",gauss=FALSE,spline=FALSE,posRange=NA,outlierSuppression=1,logfile="llr.log",iterations=10000)
 
 #set up logger and shunt it into the error handler
 logger <- new.logger(args$logfile)
@@ -57,6 +59,7 @@ if (!is.na(args$posRange)) {
   posSplit <- NA
 }
 
+logger$info("Reading input")
 #import the map
 map <- read.csv(args$map,comment.char="#")
 rownames(map) <- map$hgvs_pro
@@ -84,6 +87,7 @@ if (length(posScores) < 2 || length(negScores) < 2) {
   )
 }
 
+logger$info("Calculating densties")
 #build the LLR function
 if (args$gauss) {
   llrObj <- buildLLR.gauss(posScores,negScores,args$spline)
@@ -91,12 +95,14 @@ if (args$gauss) {
   llrObj <- buildLLR.kernel(posScores,negScores,bw=args$bandwidth,kernel=args$kernel,outlierSuppression=args$outlierSuppression)
 }
 
+logger$info("Drawing LLR plot")
 #draw the LLR function
 pdf(paste0(outprefix,".pdf"),5,5)
 drawDensityLLR(map$score,llrObj$llr,llrObj$posDens,llrObj$negDens,posScores,negScores)
-dev.off()
+invisible(dev.off())
 
 #calculate all LLRs and their confidence intervals
+logger$info("Bootstrapping confidence intervals at ",args$iterations," iterations")
 mapLLR <- llrObj$llr(map$score) 
 bootstrapScores <- Map(rnorm, args$iterations, map$score, map$se)
 scoreLLR <- sapply(bootstrapScores, llrObj$llr) 
@@ -106,16 +112,58 @@ out <- data.frame(map,llr=mapLLR,llrCI=sprintf("[%.03f;%.03f]",left,right))
 
 #write result to file
 outfile <- paste0(outprefix,".csv")
+logger$info("Writing output to file ",outfile)
 write.csv(out,outfile,row.names=FALSE)
 
 #generate a PRC curve
+logger$info("Drawing PRC curve")
 yrobj <- yr2(
   truth=c(rep(TRUE,length(posScores)),rep(FALSE,length(negScores))),
   scores=data.frame(map=c(posScores,negScores)),high=FALSE
 )
 pdf(paste0(outprefix,"_prc.pdf"),7,7)
-draw.prc.CI(yrobj,balanced=TRUE,names="map")
-dev.off()
+draw.prc.CI(yrobj,balanced=TRUE)
+invisible(dev.off())
+
+#print category transitions
+if (args$printTransitions) {
+
+  logger$info("Category transitions: ")
+  optiLLR <- function(prior=0.1,posterior=0.9) {
+    log10(posterior*(1-prior)/(prior*(1-posterior)))*4/3
+  }
+  llrThresholds <- function(LLRpvst=optiLLR(0.1),X=2) {
+    c(
+      patho.vstrong=LLRpvst,
+      patho.strong=LLRpvst/X,
+      patho.moderate=LLRpvst/(X^2),
+      patho.support=LLRpvst/(X^3),
+      benign.support=-LLRpvst/(X^3),
+      benign.strong=-LLRpvst/(X^1)
+    )
+  }
+  #TODO: Export this from maveLLR instead of re-implementing abve
+  llrTs <- llrThresholds()
+  llrTs <- c(llrTs[1:4],none=0,llrTs[5:6])
+
+  xs <- seq(min(map$score,na.rm=TRUE),max(map$score,na.rm=TRUE),length.out=1000)
+  ys <- llrObj$llr(xs)
+  layers <- sapply(ys,function(y) {
+    if (y > 0) {
+      head(names(which(llrTs < y)),1)
+    } else if (y < 0) {
+      tail(names(which(llrTs > y)),1)
+    } else {
+      "none"
+    }
+  })
+  for (i in 2:length(layers)) {
+    if (layers[[i]] != layers[[i-1]]) {
+      cat(sprintf("%.03f : %s -> %s\n",xs[[i]],layers[[i-1]],layers[[i]]))
+    }
+  }
+
+}
 
 
-
+logger$info("Done!")
