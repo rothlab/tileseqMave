150ffaa1799c315eacc8769f1b34b751af373dd9
Jochen Weile <jochenweile@gmail.com>
2023-11-23 16:38:21 -0500

    more mutcount dev

--- inst/scripts/mutcount.sh
+++ inst/scripts/mutcount.sh
@@ -242,17 +242,6 @@ if [[ -n "$MISSING" ]]; then
   exit 1
 fi
 
-#Find the appropriate WT sample for each non-WT sample
-CONDEFS="$(extractJSON $PARAMETERS conditions/definitions)"
-declare -a SWTCOND
-for ((i=0; i<${#SIDS[@]}; i++)); do
-  WTCONDNAME=$(echo "$CONDEFS" | grep "is_wt_control_for ${SCONDS[$i]}" | awk '{print $1}')
-  if [[ -n $WTCONDNAME ]]; then
-    #find the row indices that match all the following fields:
-    j=$(intersect "$(matches "SCONDS" $WTCONDNAME)" "$(matches "STILES" ${STILES[$i]})" "$(matches "STPS" "${STPS[$i]}")" "$(matches "SREPS" ${SREPS[$i]})")
-    SWTCOND[$i]=${SIDS[$j]}
-  fi
-done
 
 # Create reference libraries
 REFDIR="${OUTDIR}/ref"
@@ -289,93 +278,212 @@ extractJobID() {
   echo "${RETVAL##* }"
 }
 
-SAMDIR="${OUTDIR}/sam_files"
-mkdir -p ${SAMDIR}
-JOBS=""; JOBTAGS=""
+#helper function to submit alignment jobs
+submitAlignments() {
+  JOBTAGS="$1"
+  SAMDIR="$2"
+  JOBS="" 
+  # for ((i=0; i<${#SIDS[@]}; i++)); do
+  for JOBTAG in $JOBTAGS; do
+    if [[ "$JOBTAG" == *phix* ]]; then
+      if [[ "$JOBTAG" == *R1 ]]; then
+        LOGFILE="${SAMDIR}/bowtie_phix_R1.log"
+        SAMFILE="${SAMDIR}/phix_R1.sam"
+        FQ="$PHIXR1"
+      elif [[ "$JOBTAG" == *R2 ]]; then
+        LOGFILE="${SAMDIR}/bowtie_phix_R2.log"
+        SAMFILE="${SAMDIR}/phix_R2.sam"
+        FQ="$PHIXR2"
+      else 
+        echo "Invalid job tag. Report this as a bug!">&2
+        exit 100
+      fi
+      RETVAL=$(submitjob.sh -n "bowtiePhix" -c ${CPUS} -m 1G \
+        -l $LOGFILE -e $LOGFILE $CONDAARG $QUEUEARG --report -- \
+        bowtie2 --no-unal --no-head --no-sq --rdg 12,1 --rfg 12,1 \
+        --local --threads ${CPUS} -x "${PHIXFASTA%.fasta}" \
+        -U "$FQ" -S "$SAMFILE" )
+      JOBS=$(cons $(extractJobID "$RETVAL") "$JOBS" ",")
+    else
+      i="${JOBTAG%%_*}"
+      if [[ "$JOBTAG" == *R1 ]]; then
+        LOGFILE="${SAMDIR}/bowtie_${SIDS[$i]}_R1.log"
+        SAMFILE="${SAMDIR}/${SIDS[$i]}_R1.sam"
+        FQ="${R1S[$i]}"
+        FWREV="--norc"
+      elif [[ "$JOBTAG" == *R2 ]]; then
+        LOGFILE="${SAMDIR}/bowtie_${SIDS[$i]}_R2.log"
+        SAMFILE="${SAMDIR}/${SIDS[$i]}_R2.sam"
+        FQ="${R2S[$i]}"
+        FWREV="--nofw"
+      else 
+        echo "Invalid job tag. Report this as a bug!">&2
+        exit 100
+      fi
+      #profiling showed that bowtie uses < 1GB RAM even with 8 threads
+      RETVAL=$(submitjob.sh -n "bowtie${SIDS[$i]}" -c ${CPUS} -m 1G \
+        -l $LOGFILE -e $LOGFILE $CONDAARG $QUEUEARG --report -- \
+        bowtie2 --no-head ${FWREV} --no-sq --rdg 12,1 --rfg 12,1 --local \
+        --threads ${CPUS} -x "${REFFASTA%.fasta}" -U "$FQ" -S "$SAMFILE" )
+      JOBS=$(cons $(extractJobID "$RETVAL") "$JOBS" ",")
+    fi
+  done
+  echo "$JOBS"
+}
+
+#helper function to check if any jobs have failed
+findFailedAlignments() {
+  TAGS="$1"
+  FAILEDJOBTAGS=""
+  for JOBTAG in $TAGS; do
+    LOGFILE="${SAMDIR}/bowtie_${JOBTAG#*_}.log"
+    LASTLOGLINE=$(tail -1 $LOGFILE)
+    if [[ "$LASTLOGLINE" != "Job completed successfully." ]]; then
+      FAILEDJOBTAGS="$(cons "$JOBTAG" "$FAILEDJOBTAGS" ' ')"
+    fi
+  done
+  echo "$FAILEDJOBTAGS"
+}
+
+#Assemble list of alignment jobs to submit
+JOBTAGS=""
 for ((i=0; i<${#SIDS[@]}; i++)); do
-  LOGFILE="${SAMDIR}/bowtie_${SIDS[$i]}_R1.log"
-  SAMFILE="${SAMDIR}/${SIDS[$i]}_R1.sam"
-  RETVAL=$(submitjob.sh -n "bowtie${SIDS[$i]}" \
-    -c ${CPUS} -m 1G -l $LOGFILE -e $LOGFILE $CONDAARG $QUEUEARG -- \
-    bowtie2 --no-head --norc --no-sq --rdg 12,1 --rfg 12,1 --local --threads ${CPUS}\
-    -x "${REFFASTA%.fasta}" -U "${R1S[$i]}" -S "$SAMFILE" )
-  cons $(extractJobID "$RETVAL") "$JOBS" ","
-  cons "${SIDS[$i]}_R1" "$JOBTAGS" " "
-
-  LOGFILE="${SAMDIR}/bowtie_${SIDS[$i]}_R2.log"
-  SAMFILE="${SAMDIR}/${SIDS[$i]}_R2.sam"
-  #profiling showed that bowtie uses < 1GB RAM even with 8 threads
-  RETVAL=$(submitjob.sh -n "bowtie${SIDS[$i]}" \
-    -c ${CPUS} -m 1G -l $LOGFILE -e $LOGFILE $CONDAARG $QUEUEARG -- \
-    bowtie2 --no-head --nofw --no-sq --rdg 12,1 --rfg 12,1 --local --threads ${CPUS}\
-    -x "${REFFASTA%.fasta}" -U "${R2S[$i]}" -S "$SAMFILE" )
-  cons $(extractJobID "$RETVAL") "$JOBS" ","
-  cons "${SIDS[$i]}_R2" "$JOBTAGS" " "
+  JOBTAGS="$(cons "${i}_${SIDS[$i]}_R1 ${i}_${SIDS[$i]}_R2" "$JOBTAGS" ' ')"
 done
-
 if [[ "$USEPHIX" == "1" ]]; then
-  LOGFILE="${SAMDIR}/bowtie_phix_R1.log"
-  SAMFILE="${SAMDIR}/phix_R1.sam"
-  RETVAL=$(submitjob.sh -n "bowtiePhixR1" \
-    -c ${CPUS} -m 1G -l $LOGFILE -e $LOGFILE $CONDAARG $QUEUEARG -- \
-    bowtie2 --no-unal --no-head --no-sq --rdg 12,1 --rfg 12,1 --local --threads ${CPUS}\
-    -x "${PHIXFASTA%.fasta}" -U "$PHIXR1" -S "$SAMFILE" )
-  cons $(extractJobID "$RETVAL") "$JOBS" ","
-  cons "phix_R1" "$JOBTAGS" " "
-
-  LOGFILE="${SAMDIR}/bowtie_phix_R2.log"
-  SAMFILE="${SAMDIR}/phix_R2.sam"
-  RETVAL=$(submitjob.sh -n "bowtiePhixR2" \
-    -c ${CPUS} -m 1G -l $LOGFILE -e $LOGFILE $CONDAARG $QUEUEARG -- \
-    bowtie2 --no-unal --no-head --no-sq --rdg 12,1 --rfg 12,1 --local --threads ${CPUS}\
-    -x "${PHIXFASTA%.fasta}" -U "$PHIXR2" -S "$SAMFILE" )
-  cons $(extractJobID "$RETVAL") "$JOBS" ","
-  cons "phix_R2" "$JOBTAGS" " "
+  JOBTAGS="$(cons "NA_phix_R1 NA_phix_R2" "$JOBTAGS" ' ')"
 fi
 
+#submit alignment jobs
+SAMDIR="${OUTDIR}/sam_files"
+mkdir -p "${SAMDIR}"
+JOBS="$(submitAlignments "$JOBTAGS" "$SAMDIR")"
 waitForJobs.sh -v "$JOBS"
+
+#find any failed jobs and re-run them up to 3 times
+FAILEDJOBTAGS="$(findFailedAlignments "$JOBTAGS")"
+RETRIES=0
+while [[ -n "$FAILEDJOBTAGS" && "$RETRIES" -lt 3 ]]; do
+  ((RETRIES++))
+  echo "WARNING: $(echo $FAILEDJOBTAGS|wc -w) alignment jobs failed and will be re-submitted."
+  JOBS="$(submitAlignments "$FAILEDJOBTAGS" "$SAMDIR")"
+  waitForJobs.sh -v "$JOBS"
+  FAILEDJOBTAGS="$(findFailedAlignments "$FAILEDJOBTAGS")"
+done
+#if there are still failed jobs left, throw an error
+if [[ -n "$FAILEDJOBTAGS" ]]; then
+  echo "ERROR: The following alignment jobs failed and have exhausted all re-tries: $FAILEDJOBTAGS">&2
+  exit 1
+fi
+
 
 #PHASE 2: Run calibrateQC jobs
 echo "Running QC calibrations."
 
-CALIBDIR="${OUTDIR}/calibrations/"
-mkdir -p "$CALIBDIR"
+submitCalibrations() {
+  TAGS="$1"
+  OUTDIR="$2"
+  for JOBTAG in $TAGS; do
+      PREFIX="${JOBTAG#*_}"
+      OUT="${OUTDIR}/${PREFIX}.csv"
+      LOG="${OUTDIR}/${PREFIX}.log"
+      LOG2="${OUTDIR}/${PREFIX}_internal.log"
+      SAMFILE="${SAMDIR}/${PREFIX}.sam"
+      RETVAL=$(submitjob.sh -n "calibrate${SIDS[$i]}R1" -c 8 -m 1G \
+        -l "$LOG" -e "$LOG" $CONDAARG $QUEUEARG --report -- \
+        tsm calibratePhred "$SAMFILE" -p "$PARAMETERS" \
+        -o "$OUT" -l "$LOG2" --cores 8)
+      #extract job ID from return value and add to jobs list
+      JOBS="$(cons $(extractJobID "$RETVAL") $JOBS ',')"
+    fi
+  done
+}
 
-JOBS=""
+#helper function to check if any jobs have failed
+findFailedCalibrations() {
+  TAGS="$1"
+  LOGDIR="$2"
+  FAILEDJOBTAGS=""
+  for JOBTAG in $TAGS; do
+    LOGFILE="${LOGDIR}/${JOBTAG#*_}.log"
+    LASTLOGLINE=$(tail -1 $LOGFILE)
+    if [[ "$LASTLOGLINE" != "Job completed successfully." ]]; then
+      FAILEDJOBTAGS="$(cons "$JOBTAG" "$FAILEDJOBTAGS" ' ')"
+    fi
+  done
+  echo "$FAILEDJOBTAGS"
+}
 
-if [[ "$USEPHIX" == "0" ]]; then
+#Find the appropriate WT sample for each non-WT sample
+CONDEFS="$(extractJSON $PARAMETERS conditions/definitions)"
+declare -a SWTCOND
+for ((i=0; i<${#SIDS[@]}; i++)); do
+  WTCONDNAME=$(echo "$CONDEFS" | grep "is_wt_control_for ${SCONDS[$i]}" | awk '{print $1}')
+  if [[ -n $WTCONDNAME ]]; then
+    #find the row indices that match all the following fields:
+    j=$(intersect "$(matches "SCONDS" $WTCONDNAME)" "$(matches "STILES" ${STILES[$i]})" "$(matches "STPS" "${STPS[$i]}")" "$(matches "SREPS" ${SREPS[$i]})")
+    SWTCOND[$i]=${SIDS[$j]}
+  fi
+done
+
+#Assemble list of calibration jobs to be run later
+JOBTAGS=""
+if [[ "$USEPHIX" == 1 ]]; then
+  JOBTAGS="NA_phix_R1 NA_phix_R2"
+else # i.e. if USEPHIX=0
   WTCONDNAMES=$(grep "is_wt_control_for" < <(echo "$CONDEFS")|awk '{print $1}'|sort|uniq)
   for WTCONDNAME in $WTCONDNAMES; do
     for i in $(matches SCONDS $WTCONDNAME); do
-      # echo "${R1S[$i]} ${R2S[$i]}"
-      #tsm calibratePhred {wt_r1_sam} -p {self._param} -o {phred_output_r1} -l {log_f} --silent --cores {self._cores}
-      JOBOUT="${CALIBDIR}/${SIDS[$i]}_R1.csv"
-      JOBLOG="${CALIBDIR}/${SIDS[$i]}_R1.log"
-      JOBLOGINT="${CALIBDIR}/${SIDS[$i]}_R1_internal.log"
-      RETVAL=$(submitjob.sh -n "calibrate${SIDS[$i]}R1" -c 8 -m 1G \
-        -l "$JOBLOG" -e "$JOBLOG" $CONDAARG $QUEUEARG -- \
-        tsm calibratePhred "${R1S[$i]}" -p "$PARAMETERS" \
-        -o "$JOBOUT" -l "$JOBLOGINT" --cores 8)
-      #extract job ID from return value and add to jobs list
-      cons $(extractJobID "$RETVAL") $JOBS ","
-
-      JOBOUT="${CALIBDIR}/${SIDS[$i]}_R2.csv"
-      JOBLOG="${CALIBDIR}/${SIDS[$i]}_R2.log"
-      JOBLOGINT="${CALIBDIR}/${SIDS[$i]}_R2_internal.log"
-      RETVAL=$(submitjob.sh -n "calibrate${SIDS[$i]}R2" -c 8 -m 1G \
-        -l "$JOBLOG" -e "$JOBLOG" $CONDAARG $QUEUEARG -- \
-        tsm calibratePhred "${R2S[$i]}" -p "$PARAMETERS" \
-        -o "$JOBOUT" -l "$JOBLOGINT" --cores 8)
-      #extract job ID from return value and add to jobs list
-      cons $(extractJobID "$RETVAL") $JOBS ","
+      JOBTAGS="$(cons "${i}_${SIDS[$i]}_R1 ${i}_${SIDS[$i]}_R2" "$JOBTAGS" ' ')"
     done
   done
-else # if USEPHIX==1
-  ###########################
-  # TODO: Implement PhiX here
-  ###########################
 fi
+
+#make a directory for the calibration output
+CALIBDIR="${OUTDIR}/calibrations/"
+mkdir -p "$CALIBDIR"
+JOBS="$(submitCalibrations "$JOBTAGS" "$CALIBDIR")"
 waitForJobs.sh -v "$JOBS"
+
+#find any failed jobs and re-run them up to 3 times
+FAILEDJOBTAGS="$(findFailedCalibrations "$JOBTAGS" "$CALIBDIR")"
+RETRIES=0
+while [[ -n "$FAILEDJOBTAGS" && "$RETRIES" -lt 3 ]]; do
+  ((RETRIES++))
+  echo "WARNING: $(echo $FAILEDJOBTAGS|wc -w) calibration jobs failed and will be re-submitted."
+  JOBS="$(submitCalibrations "$FAILEDJOBTAGS" "$CALIBDIR")"
+  waitForJobs.sh -v "$JOBS"
+  FAILEDJOBTAGS="$(findFailedAlignments "$FAILEDJOBTAGS" "$CALIBDIR")"
+done
+#if there are still failed jobs left, throw an error
+if [[ -n "$FAILEDJOBTAGS" ]]; then
+  echo "ERROR: The following calibration jobs failed and have exhausted all re-tries: $FAILEDJOBTAGS">&2
+  exit 1
+fi
+
+
+
+      # #tsm calibratePhred {wt_r1_sam} -p {self._param} -o {phred_output_r1} -l {log_f} --silent --cores {self._cores}
+      # JOBOUT="${CALIBDIR}/${SIDS[$i]}_R1.csv"
+      # JOBLOG="${CALIBDIR}/${SIDS[$i]}_R1.log"
+      # JOBLOGINT="${CALIBDIR}/${SIDS[$i]}_R1_internal.log"
+      # RETVAL=$(submitjob.sh -n "calibrate${SIDS[$i]}R1" -c 8 -m 1G \
+      #   -l "$JOBLOG" -e "$JOBLOG" $CONDAARG $QUEUEARG -- \
+      #   tsm calibratePhred "${R1S[$i]}" -p "$PARAMETERS" \
+      #   -o "$JOBOUT" -l "$JOBLOGINT" --cores 8)
+      # #extract job ID from return value and add to jobs list
+      # cons $(extractJobID "$RETVAL") $JOBS ","
+
+      # JOBOUT="${CALIBDIR}/${SIDS[$i]}_R2.csv"
+      # JOBLOG="${CALIBDIR}/${SIDS[$i]}_R2.log"
+      # JOBLOGINT="${CALIBDIR}/${SIDS[$i]}_R2_internal.log"
+      # RETVAL=$(submitjob.sh -n "calibrate${SIDS[$i]}R2" -c 8 -m 1G \
+      #   -l "$JOBLOG" -e "$JOBLOG" $CONDAARG $QUEUEARG -- \
+      #   tsm calibratePhred "${R2S[$i]}" -p "$PARAMETERS" \
+      #   -o "$JOBOUT" -l "$JOBLOGINT" --cores 8)
+      # #extract job ID from return value and add to jobs list
+      # cons $(extractJobID "$RETVAL") $JOBS ","
+
 
 
 #PHASE 3: Run variant calling jobs and monitor
